078d18b9c105811204aa404be6a02980a08934ea
fuji.nguyen <fuji.nguyen@live.com>
2019-03-22 23:08:33 -0400

    Init check in

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { finalize } from 'rxjs/operators';
+
+import { QuoteService } from './quote.service';
+
+import { HttpClient } from '@angular/common/http';
+import { FileSaverService } from 'ngx-filesaver';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  quote: string;
+  isLoading: boolean;
+  name = 'Angular 5';
+  fileUrl: any;
+  public fileName: string;
+
+  constructor(private quoteService: QuoteService, private _httpClient: HttpClient, private _FileSaverService: FileSaverService) { }
+
+  ngOnInit() {
+    this.isLoading = true;
+    this.quoteService.getRandomQuote({ category: 'dev' })
+      .pipe(finalize(() => { this.isLoading = false; }))
+      .subscribe((quote: string) => { this.quote = quote; });
+  }
+  onDown(type: string, fromRemote: boolean) {
+    const fileName = `EmployeeProfile.${type}`;
+    if (fromRemote) {
+      this._httpClient.get(`http://localhost:11444/api/Details/Report/DownloadReport`, {
+        observe: 'response',
+        responseType: 'blob'
+      }).subscribe(res => {
+        this._FileSaverService.save(res.body, fileName);
+      });
+      return;
+    }
+  }
+}
